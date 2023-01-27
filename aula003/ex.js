40084d11e0ad2b8ad2d0145acd7c0793301dc667
Valdemar Santana <80076890+Valdemar1213@users.noreply.github.com>
2023-01-27 20:15:30 -0300

    Aula 3 de JS Udemy

---
+++ aula003/ex.js
@@ -0,0 +1,4 @@
+var nome = 'Valdemar'
+console.log(`Meu nome é ${nome}. Estou aprendendo JavaScript às 10 da manhã.`)
+
+console.log('Meu nome é "Valdemar". Estou aprendendo JavaScript às', 10, 'da manhã')
---
+++ aula003/index.js
@@ -0,0 +1,4 @@
+console.log('Olá mundo!'); // String = Texto
+
+
+console.log(35, 15.85, 'Valdemar Santana'); //Number = Número
