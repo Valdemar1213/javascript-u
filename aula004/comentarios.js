ab73ebd2eefe968903986f140998e00805062d2a
Valdemar Santana <80076890+Valdemar1213@users.noreply.github.com>
2023-01-27 20:27:45 -0300

    Aula 04 Comentários

---
+++ aula004/comentarios.js
@@ -0,0 +1,14 @@
+// Comentário em JavaScript.
+console.log('Olá mundo'); // Aqui tem outro comentário.
+console.log('Linha 1');
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+// Vai ser ignorado.
+
+// Comentário em linha única
+/* 
+    Comentário 
+        em
+      Bloco 
+*/ 
