a1c5b824c914a8bd3786eb1e7c886d97dc31bd6b
Valdemar Santana <80076890+Valdemar1213@users.noreply.github.com>
2023-01-27 21:22:04 -0300

    Aula 06 Regras e concatenação simples

---
+++ aula006/index1.js
@@ -0,0 +1,5 @@
+let nome; // Apenas declarou a variável
+nome = 'João'; // Inicializando a variável
+console.log(nome);
+nome = 'Valdemar';
+console.log(nome)
---
+++ aula006/regras.js
@@ -0,0 +1,8 @@
+// Não podemos variáveis com palavras reservadas pelo JS
+// Variáveis precisam ter nome com significados
+// Não pode começar o nome de uma variável com número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase 
+// Case-sensitive (Maiúsculas e minúsculas tem diferença)
+// Não podemos redeclarar variáveis com 'let'
+// NÃO UTILIZE 'VAR', UTILIZE 'LET'
