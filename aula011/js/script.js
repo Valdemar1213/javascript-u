c35e9f4b99d5934870ee5da50f104db67faf4c36
Valdemar Santana <80076890+Valdemar1213@users.noreply.github.com>
2023-01-28 18:41:51 -0300

    Alert, confirm e prompt Aplicação e exercício

---
+++ aula011/js/script.js
@@ -0,0 +1,12 @@
+// const confirma = confirm('Deseja realmente apagar?')
+//let confirma = confirm('Confirma?')
+
+/* window.alert('Olá, mundo!');
+window.confirm('Está gostando do curso?')
+window.prompt('Qual seu nome?')
+*/
+
+let num1 = prompt('Digite um número')
+let num2 = prompt('Digite outro número')
+let resultado = Number(num1) + Number(num2)
+alert(`O resultado foi ${resultado}.`)
