645a36cdb3264be1785b4c5a391f720993432b69
Valdemar Santana <80076890+Valdemar1213@users.noreply.github.com>
2023-01-28 18:12:01 -0300

    Operadores

---
+++ aula010/index.js
@@ -0,0 +1,33 @@
+/* 
+ * Aritméticos + - / *
+ * + Adição / Concatenação 
+ * - / *
+ * ** Potenciação
+ * % Resto da divisão
+*/
+
+/* Precedência
+()
+**
+* / %
++ -
+*/
+
+const num1 = 5;
+const num2 = 10;
+console.log(num1 + num2);
+
+// Operadores de Incremento = ++ / Operador Decremento = --
+
+let contador = 1; // Incremento apenas com LET
+contador++; // 2
+contador++; // 3
+console.log(++contador);
+
+// Incremento ou Decremento de mais de 1 valor
+
+// Operadores de Atribuição
+
+let contador1 = 2;
+contador1 *= 2; // contador1 = contador1 + 50
+console.log(contador1);
---
+++ aula010/op.js
@@ -0,0 +1,12 @@
+// NaN = Not A Number / parseInt (Inteiro) / parseFloat (Decimal)
+
+const num1 = 10;
+const num2 = parseInt('5');
+const num3 = parseFloat('5.2');
+const num4 = Number('5.5');
+console.log(num1 + num2);
+console.log(num1 + num3);
+console.log(num1 + num4);
+console.log(typeof num2);
+console.log(typeof num3);
+console.log(typeof num4);
