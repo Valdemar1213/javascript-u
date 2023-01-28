1dbd058d7c6fb73a56266523038c064085453726
Valdemar Santana <80076890+Valdemar1213@users.noreply.github.com>
2023-01-28 18:44:09 -0300

    Correções do exercicio

--- aula011/js/script.js
+++ aula011/js/script.js
@@ -6,7 +6,7 @@ window.confirm('Está gostando do curso?')
 window.prompt('Qual seu nome?')
 */
 
-let num1 = prompt('Digite um número')
-let num2 = prompt('Digite outro número')
-let resultado = Number(num1) + Number(num2)
-alert(`O resultado foi ${resultado}.`)
+let num1 = prompt('Digite um número');
+let num2 = prompt('Digite outro número');
+let resultado = Number(num1) + Number(num2);
+alert(`O resultado da soma foi ${resultado}.`);
