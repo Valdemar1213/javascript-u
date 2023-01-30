489bd16af82582c85c928b2f7a656f8905c05e02
Valdemar Santana <80076890+Valdemar1213@users.noreply.github.com>
2023-01-30 15:49:19 -0300

    Lógica de Programação JS Operadores

---
+++ aula020/index.js
@@ -0,0 +1,13 @@
+/*
+Operadores de comparação
+> Maior que
+>= Maior ou igual a (Se um dos resultados é true ... da Verdadeiro)
+< Menor que
+<= Menos ou igual a (Se um dos resultados é true ... da Verdadeiro)
+== Igualdade (Valor) *** (Não usar !!)
+=== Igualdade estrita (Valor e Tipo)
+!= Diferente (Valor) *** (Não usar !!)
+!== Diferente estrito (Valor e Tipo)
+*/
+const comp = 10 >= 5;
+console.log(comp);
---
+++ aula021/index.js
@@ -0,0 +1,20 @@
+/*
+Operadores Lógicos
+&& -> AND -> E -> Todas as espressões precisam ser verdadeiro para retornar true
+|| -> OR -> OU -> Se apenas 1 delas for verdadeiro, retornará verdadeiro
+! -> NOT -> NÃO
+*/
+
+// const expressaoAnd = true && true && true && true;
+// const expressaoOr = false || true;
+
+/*
+const usuario = 'Valdemar';
+const senha = '123456';
+
+//                      True                    False
+const vaiLogar = usuario === 'Valdemar' && senha === '12345';
+console.log(vaiLogar);
+*/
+
+console.log(!true);
