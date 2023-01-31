ff08b0545c75450ba72ddb5c86b387321a34aac2
Valdemar Santana <80076890+Valdemar1213@users.noreply.github.com>
2023-01-31 19:24:40 -0300

    Diferenças var e let

---
+++ ex028/index.js
@@ -0,0 +1,45 @@
+const verdadeira = true
+
+/* let nome = 'Valdemar'; // Criando
+var nome2 = 'Valdemar'; // Criando
+
+var nome2 = 'Fernanda'; // Redeclarada
+
+
+// let tem escopo de bloco { BLOCO }
+// var só tem escopo de função
+
+if (verdadeira) {
+    let nome = 'Pedro'; // Criando
+    var nome2 = 'Maria'; // Redeclarando
+    //console.log(nome, nome2);
+
+    if (verdadeira) {
+        var nome2 = 'Genilda'; // Redeclarando
+        let nome = 'Outra coisa'; // Criando
+    }
+}
+
+console.log(nome, nome2); */
+
+/* var sobrenome = 'Santana';
+
+function falaOi() {
+    var nome = 'Valdemar';
+    console.log(nome, sobrenome);
+
+    if (verdadeira) {
+        let nome = 'Pedro';
+        console.log(nome);
+    }
+}
+
+falaOi(); */
+
+function nomeFuncao() {
+    
+}
+
+let sobrenome = 'Santana';
+console.log(sobrenome);
+
