b3eaef049b6cf76f617b15acdd9163c087ee5cdc
Valdemar Santana <80076890+Valdemar1213@users.noreply.github.com>
2023-02-04 15:44:23 -0300

    Splice

---
+++ aula052/index.js
@@ -0,0 +1,13 @@
+//                -5        -4         -3         -2      -1
+//                 0         1          2          3       4
+const nomes = ['Valdemar', 'Pedro', 'Fernanda', 'Fernando', 'Genilda'];
+nomes.splice(0, 0, 'Gabriela', 'Dida', 'Otávio', 'Miranda');
+
+
+console.log(nomes);
+
+// nomes.splice(índice atual, delete, elem1, elem2, elem3);
+// pop 
+/* const removidos = nomes.splice(-1, 1); */
+
+// const removidos = nomes.splice(0, 1);
