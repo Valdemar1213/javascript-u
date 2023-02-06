0f3c440c26123c725f12770ea64c956429e83623
Valdemar Santana <80076890+Valdemar1213@users.noreply.github.com>
2023-02-06 15:49:45 -0300

    Criação de class

---
+++ aula070/index.js
@@ -0,0 +1,24 @@
+// Classes === Function
+
+class Pessoa {
+  constructor(nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+  }
+
+  falar() {
+    console.log(`${this.nome} está falando.`);
+  }
+}
+
+function Pessoa2(nome, sobrenome) {
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+}
+
+Pessoa2.prototype.falar = function() {
+  console.log(`${this.nome} está falando.`);
+};
+
+const p1 = new Pessoa('Valdemar', 'Santana');
+const p2 = new Pessoa2('Pedro', 'Santana');
