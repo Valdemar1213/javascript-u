4feee3d421c758a57277b346076e04b58cdd1aab
Valdemar Santana <80076890+Valdemar1213@users.noreply.github.com>
2023-02-09 17:44:35 -0300

    Arrumando o utf8

--- node/aula5/modules/ler.js
+++ node/aula5/modules/ler.js
@@ -1,6 +1,6 @@
 const fs = require("fs").promises;
 
-module.exports = (caminho) => fs.readFile(caminho, 'utf-8');
+module.exports = (caminho) => fs.readFile(caminho, 'utf8');
 
 /* module.exports = (caminho, dados) => {
   fs.writeFile(caminho, dados, { flag: "w" });
