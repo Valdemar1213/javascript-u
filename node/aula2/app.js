776f9189b4bd347133483c29a762a00388943d5c
Valdemar Santana <80076890+Valdemar1213@users.noreply.github.com>
2023-02-09 16:19:08 -0300

    Modules dirname filename

---
+++ node/aula2/app.js
@@ -0,0 +1,21 @@
+// const Cachorro = require('./b/c/d/mod');
+/* const Cachorro = require('./z/mod2')
+
+const dog = new Cachorro('Suri');
+dog.latir(); */
+/* console.log(__filename);
+
+console.log(__dirname); */
+
+
+/* const path = require('path');
+console.log(path.resolve(__dirname, '.', '.', 'arquivos', 'imagens'));
+ */
+
+const Cachorro = require('./z/mod2');
+
+const c1 = new Cachorro('Suri');
+c1.latir();
+
+
+
