5c0f28e9cee68dd40c9c55b3d06cbbc0f66790ed
Valdemar Santana <80076890+Valdemar1213@users.noreply.github.com>
2023-02-10 18:52:48 -0300

    Express template arquivos estáticos

---
+++ node/aula11/routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const route = express.Router();
+const homeController = require('./src/controllers/homeController')
+const contatoController = require('./src/controllers/contatoController.js')
+
+// Rotas da HOME
+route.get("/", homeController.paginaInicial);
+route.post('/', homeController.trataPost);
+
+// Rotas de CONTATO
+route.get('/contato', contatoController.paginaInicial)
+
+
+
+module.exports = route;
