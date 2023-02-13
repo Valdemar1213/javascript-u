14e7e3035875db9177bdc17e211ccb629d4df01c
Valdemar Santana <80076890+Valdemar1213@users.noreply.github.com>
2023-02-13 18:41:58 -0300

    Helmet e csrf

--- node/aula13/server.js
+++ node/aula13/server.js
@@ -1,25 +1,22 @@
 require('dotenv').config();
-
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
-
 mongoose.connect(process.env.CONNECTIONSTRING).then(() => {
   app.emit('pronto');
 }).catch(e => console.log(e));
-
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
-
-
 const routes = require('./routes');
 const path = require('path');
-const { middlewareGlobal } = require('./src/middlewares/middleware')
+const helmet = require('helmet');
+const csrf = require('csurf');
+const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware')
 
+app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 const sessionOptions = session({
@@ -38,8 +35,11 @@ app.use(flash());
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
+app.use(csrf());
 // Nossos próprios middlewares
 app.use(middlewareGlobal);
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
 app.use(routes);
 
 app.on('pronto', () => {
