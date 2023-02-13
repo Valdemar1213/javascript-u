b92415d11a0ed46222dc6e3fd4d3e59499ed5377
Valdemar Santana <80076890+Valdemar1213@users.noreply.github.com>
2023-02-13 17:43:18 -0300

    Express session e flash message

--- node/aula13/server.js
+++ node/aula13/server.js
@@ -9,6 +9,11 @@ mongoose.connect(process.env.CONNECTIONSTRING).then(() => {
   app.emit('pronto');
 }).catch(e => console.log(e));
 
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+
+
 const routes = require('./routes');
 const path = require('path');
 const { middlewareGlobal } = require('./src/middlewares/middleware')
@@ -16,6 +21,20 @@ const { middlewareGlobal } = require('./src/middlewares/middleware')
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.resolve(__dirname, 'public')));
+
+const sessionOptions = session({
+  secret: 'texto que ninguem vai saber',
+  //store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true
+  },
+  store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING})
+});
+app.use(sessionOptions);
+app.use(flash());
 
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
